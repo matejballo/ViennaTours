a7e9f2ad4857b7d6b458a967ecd4e0592eac026c
matejballo <mattej.ballo@gmail.com>
2021-01-02 11:53:08 +0100

    Added admin static pages

--- Admin/urls.py
+++ Admin/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.dashboard, name='admin-dashboard'),
+    path('employees/', views.employees, name='admin-employees'),
+    path('settings/', views.settings, name='admin-settings'),
+    path('vehicles/', views.vehicles, name='admin-vehicles'),
 ]
--- Admin/views.py
+++ Admin/views.py
@@ -3,4 +3,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'index.html')
+    return render(request, 'admin/index.html')
+
+def employees(request):
+    return render(request, 'admin/employees.html')
+
+def settings(request):
+    return render(request, 'admin/settings.html')
+
+def vehicles(request):
+    return render(request, 'admin/vehicles.html')
